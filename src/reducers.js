6e6605e9edd16e704806baf38d5cf38d9cbd6721
Scott MacLellan <s.j.maclell@gmail.com>
2016-07-23 02:50:14 -0400

    Simplifying the reducers

--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,4 @@
-function reduceCells(cells) {
+function solvedCell(cells) {
   const values = cells.map(c => c.value);
 
   let reduced = 0;
@@ -12,28 +12,14 @@ function reduceCells(cells) {
   return reduced;
 }
 
-function reduceColumn(board, column) {
-  const cells = board.column(column);
-  return reduceCells(cells);
-}
-
-function reduceRow(board, row){
-  const cells = board.row(row);
-  return reduceCells(cells);
-}
-
-function reduceSquare(board, square) {
-  const cells = board.square(square);
-  return reduceCells(cells);
-}
-
-function simpleReducers() {
-  const reducers = [];
+function reduceEach(cellReducer) {
+  const types = ['column', 'row', 'square'];
+  let reducers = [];
   for(let i = 0; i < 9; i++) {
     let v = i;
-    reducers.push(board => reduceColumn(board, v));
-    reducers.push(board => reduceRow(board, v));
-    reducers.push(board => reduceSquare(board, v));
+    reducers = reducers.concat(types.map(
+      type => board => cellReducer(board[type](v))
+    ));
   }
 
   return reducers;
@@ -41,7 +27,7 @@ function simpleReducers() {
 
 export default function reduce(board) {
   let updated = board.clone();
-  const reducers = simpleReducers();
+  const reducers = reduceEach(solvedCell);
 
   let rounds = 0;
   const combiner = function (count, reducer) {
